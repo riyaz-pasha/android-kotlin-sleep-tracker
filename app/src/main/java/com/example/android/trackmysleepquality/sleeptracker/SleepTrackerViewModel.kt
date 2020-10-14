e27703c74f81b01fb4cda3811f298f5d83ca2a6d
riyaz <riyazpasha880@gmail.com>
2020-10-14 12:23:35 +0530

    handle button visibilites based on state chages

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -51,6 +51,10 @@ class SleepTrackerViewModel(
     val navigateToSleepQuality: LiveData<SleepNight>
         get() = _navigateToSleepQuality
 
+    val startButtonVisible = Transformations.map(tonight) { null == it }
+    val stopButtonVisible = Transformations.map(tonight) { null != it }
+    val clearButtonVisible = Transformations.map(nights) { it?.isNotEmpty() }
+
     init {
         initializeTonight()
     }
